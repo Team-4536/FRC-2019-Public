87f9611ed85c075bcfbd741a9319d5baf0e30ae0
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-01-11 22:27:34 -0600

    Restructured the robot template to match Oracle naming conventions

--- src/main/java/org/minutebots/robot/Main.java
+++ src/main/java/org/minutebots/robot/Main.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package org.minutebots.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
--- src/main/java/org/minutebots/robot/Robot.java
+++ src/main/java/org/minutebots/robot/Robot.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package org.minutebots.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
