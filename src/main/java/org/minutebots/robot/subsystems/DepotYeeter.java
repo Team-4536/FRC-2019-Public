9fc006bc9196a511b1beab4444a068557744151f
Tiger2222 <Tiger2222@users.noreply.github.com>
2019-01-17 19:48:31 -0600

    Created some subsystems

---
+++ src/main/java/org/minutebots/robot/subsystems/DepotYeeter.java
@@ -0,0 +1,16 @@
+package org.minutebots.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DepotYeeter extends Subsystem {
+
+    public static DepotYeeter depotYeeter = new DepotYeeter();
+
+    private DepotYeeter(){
+
+    }
+
+    public void initDefaultCommand(){
+
+    }
+}
---
+++ src/main/java/org/minutebots/robot/subsystems/Intake.java
@@ -0,0 +1,47 @@
+package org.minutebots.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import org.minutebots.robot.RobotMap;
+
+import static org.minutebots.robot.RobotMap.Cone_2;
+
+public class Intake extends Subsystem {
+
+    private DoubleSolenoid Velcro1;
+    private DoubleSolenoid Velcro2;
+    private DoubleSolenoid Cone;
+    public static Intake intake = new Intake();
+
+    private Intake(){
+        Velcro1 = new DoubleSolenoid(RobotMap.INTAKE_1_1, RobotMap.INTAKE_1_2);
+        Velcro2 = new DoubleSolenoid(RobotMap.INTAKE_2_1, RobotMap.INTAKE_2_2);
+        Cone = new DoubleSolenoid(RobotMap.Cone_1, RobotMap.Cone_2);
+    }
+
+    public void setIntakeStatus(boolean out){
+        if (out){
+            Velcro1.set(DoubleSolenoid.Value.kForward);
+            Velcro2.set(DoubleSolenoid.Value.kForward);
+        }
+        else if (! out){
+            Velcro1.set(DoubleSolenoid.Value.kReverse);
+            Velcro2.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+
+    public void setConeStatus(boolean out){
+        if (out){
+            Cone.set(DoubleSolenoid.Value.kForward);
+        }
+        else if (!out){
+            Cone.set(DoubleSolenoid.Value.kReverse);
+        }
+    }
+
+    public void initDefaultCommand(){
+
+    }
+}
