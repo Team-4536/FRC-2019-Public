7330ad8741077defee46e23d038c8ee0b7280e92
abdulhamid6516444662 <david6516444662>
2019-01-19 09:36:59 -0600

    Added Command Shells

---
+++ src/main/java/org/minutebots/robot/commands/HatchCommand.java
@@ -0,0 +1,42 @@
+package org.minutebots.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class HatchCommand extends Command {
+
+    boolean hasPanel;
+
+    public HatchCommand(boolean hatchPanel) {
+
+        hasPanel = hatchPanel;
+
+    }
+
+    protected void initialize () {
+
+
+
+    }
+
+    protected void execute() {
+
+        if(hasPanel) {
+
+            // this is the method for realising the panel
+
+        }
+        else {
+
+            // this is the method for getting the panel
+
+        }
+
+    }
+
+    protected boolean isFinished() {
+
+        return true;
+
+    }
+
+}
