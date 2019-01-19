dd3d04a56db558fb16d822afd2d71f19010d5c6b
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-01-19 16:47:30 -0600

    fix the oi

--- src/main/java/org/minutebots/robot/OI.java
+++ src/main/java/org/minutebots/robot/OI.java
@@ -9,8 +9,9 @@ import org.minutebots.robot.subsystems.Intake;
 public class OI {
     // Input Numbers
     private static final int PRIMARY_STICK_PORT = 0,
-            EXTEND_INTAKE = 1,
-            RETRACT_INTAKE = 2,
+            TOGGLE_TURN = 1,
+            EXTEND_INTAKE = 5,
+            RETRACT_INTAKE = 3,
             ADJUST_ANGLE_LEFT = 7,
             ADJUST_ANGLE_RIGHT = 8;
 
@@ -35,16 +36,17 @@ public class OI {
 
     /*---------------------------------------Programmer's territory starts here----------------------------------*/
     public static final Joystick primaryStick = new Joystick(PRIMARY_STICK_PORT);
-    public static final JoystickButton extend = new JoystickButton(primaryStick, EXTEND_INTAKE),
+    public static final JoystickButton trigger = new JoystickButton(primaryStick, TOGGLE_TURN),
+            extend = new JoystickButton(primaryStick, EXTEND_INTAKE),
             retract = new JoystickButton(primaryStick, RETRACT_INTAKE),
             angleAdjustLeft = new JoystickButton(primaryStick, ADJUST_ANGLE_LEFT),
             angleAdjustRight = new JoystickButton(primaryStick, ADJUST_ANGLE_RIGHT);
 
-    static{
+    static {
         angleAdjustLeft.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(-3.0)));
         angleAdjustRight.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(3.0)));
         extend.whenPressed(Intake.extend());
         retract.whenPressed(Intake.retract());
     }
 
-    }
+}
