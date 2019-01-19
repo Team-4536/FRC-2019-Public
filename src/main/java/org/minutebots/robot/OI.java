f5b7f0839f466b0707887269657edd84427b3ed1
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-01-19 15:49:55 -0600

    Update OI.java

--- src/main/java/org/minutebots/robot/OI.java
+++ src/main/java/org/minutebots/robot/OI.java
@@ -4,43 +4,55 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import org.minutebots.robot.subsystems.Drivetrain;
+import org.minutebots.robot.subsystems.Intake;
 
 public class OI {
-    // Input
-    // Ports
+    // Input Numbers
+    private static final int PRIMARY_STICK_PORT = 0,
+            EXTEND_INTAKE = 1,
+            RETRACT_INTAKE = 2,
+            ADJUST_ANGLE_LEFT = 7,
+            ADJUST_ANGLE_RIGHT = 8;
 
-    public static final int PRIMARY_STICK_PORT = 0;
-    public static final int TRIGGER_BUTTON = 1;
 
-    // Objects
+    // Output Ports
+    //Talons
+    public static final int LEFT_FRONT_MOTOR = 2;
+    public static final int LEFT_BACK_MOTOR = 1;
+    public static final int RIGHT_FRONT_MOTOR = 0;
+    public static final int RIGHT_BACK_MOTOR = 3;
 
+    //Pneumatic Ports
+    public final static int INTAKE_1 = 1;
+    public final static int INTAKE_2 = 2;
+    public final static int CONE_1 = 3;
+    public final static int CONE_2 = 4;
+
+
+    //Victor Ports
+    public final static int SIDE_WHEEL = 0;
+    public final static int WHEEL = 1;
+
+    /*---------------------------------------Programmer's territory starts here----------------------------------*/
     public static final Joystick primaryStick = new Joystick(PRIMARY_STICK_PORT);
-    public static final JoystickButton trigger = new JoystickButton(primaryStick, TRIGGER_BUTTON);
+    public static final JoystickButton extend = new JoystickButton(primaryStick, EXTEND_INTAKE),
+            retract = new JoystickButton(primaryStick, RETRACT_INTAKE),
+            angleAdjustLeft = new JoystickButton(primaryStick, ADJUST_ANGLE_LEFT),
+            angleAdjustRight = new JoystickButton(primaryStick, ADJUST_ANGLE_RIGHT);
 
-    private static final JoystickButton angleAdjustLeft = new JoystickButton(primaryStick, 7);
-    private static final JoystickButton angleAdjustRight = new JoystickButton(primaryStick, 8);
-
-    // Output
-        //Talons
-        public static final int LEFT_FRONT_MOTOR = 2;
-        public static final int LEFT_BACK_MOTOR = 1;
-        public static final int RIGHT_FRONT_MOTOR = 0;
-        public static final int RIGHT_BACK_MOTOR = 3;
-
-        //Pneumatic Ports
-        public final static int INTAKE_1 = 1;
-        public final static int INTAKE_2 = 2;
-        public final static int CONE_1 = 3;
-        public final static int CONE_2 = 4;
-
-
-        //Victor Ports
-        public final static int SIDE_WHEEL = 0;
-        public final static int WHEEL = 1;
-    
     static{
         angleAdjustLeft.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(-3.0)));
         angleAdjustRight.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(3.0)));
+
+        extend.whenPressed(new InstantCommand(Intake.getInstance(), () -> {
+            Intake.getInstance().setIntakeStatus(true);
+            Intake.getInstance().setConeStatus(true);
+        }));
+
+        retract.whenPressed(new InstantCommand(Intake.getInstance(), () -> {
+            Intake.getInstance().setIntakeStatus(false);
+            Intake.getInstance().setConeStatus(false);
+        }));
     }
-    
+
     }
