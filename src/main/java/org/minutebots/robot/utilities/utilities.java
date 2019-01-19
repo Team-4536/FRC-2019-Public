b36eb6fe9788a521858b64353e691416bcec56c7
Jacob M <43586627+Jacobjme@users.noreply.github.com>
2019-01-19 13:12:54 -0600

    Proper documentation
    I added proper documentation to the utilities. :)

--- src/main/java/org/minutebots/robot/utilities/utilities.java
+++ src/main/java/org/minutebots/robot/utilities/utilities.java
@@ -12,24 +12,39 @@ public final class utilities {
     private static Timer timer = new Timer();
 
 
-    //this starts a timer
+    /**
+     * This initiates the timer object.
+     */
     public static void startTimer() {
 
         timer.start();
     }
 
-    //this gets how long the timer has been running
+    /**
+     * @return How long the timer has been running.
+     */
     public static double getTime() {
 
         return timer.get();
     }
 
-    //checks if 2 numbers are a certain distance apart.
+    /**
+     * @param a The first number
+     * @param b The second number.
+     * @param epsilon how far apart they should be
+     * @return True or false based on if the 2 numbers are epsilon apart from each other.
+     */
     public static boolean epsilonEquals(double a, double b, double epsilon) {
         return (a - epsilon <= b) && (a + epsilon >= b);
     }
 
-    //this checks if the input is within the bounds, and if it isn't, it matches to the nearest bound.
+    /**
+     * @param input Number in question
+     * @param lowerBound How low the number should be
+     * @param upperBound How high the number should be
+     * @return If the input is within the bounds it will return the input, if not it will return the nearest bound.
+     */
+
     public static double limit(double input, double lowerBound, double upperBound) {
 
         if (input < lowerBound)
@@ -41,14 +56,22 @@ public final class utilities {
     }
 
 
-    //check if input is within an upper bound and the upper bound *-1
+    /**
+     * @param input Number in question.
+     * @param bound This is the upper bound, the lower bound is bound*-1.
+     * @return If the input is within the bounds it will return the input, if not it will return the nearest bound.
+     */
     public static double limit(double input, double bound) {
 
         return limit(input, -bound, bound);
     }
 
 
-    //input value raised an exponent (curved)
+    /**
+     * @param input Number being affected.
+     * @param curve Power that the input is raised to.
+     * @return input to the power of curve
+     */
     public static double speedCurve(double input, double curve) {
         //negative curves cause asymptotes, leading to overflow errors. Curves smaller than 0.1 aren't very useful.
         double adjustedCurve = limit(curve, 0.10, Double.MAX_VALUE);
@@ -61,7 +84,10 @@ public final class utilities {
         return Math.pow(adjustedInput, adjustedCurve);
     }
 
-    //input needs to be at a certain magnitude, if not return 0, if it is return input
+    /**
+     * @param input Number that is being checked.
+     * @return If input is outside of deadzone, return input. If not return 0.0.
+     */
     static double deadZone(double input) {
 
         if ((input > -0) && (input < 0))  //needs a constant where 0 is, keep the negative on the first one
@@ -70,7 +96,12 @@ public final class utilities {
             return input;
     }
 
-    //this limits how fast you can accelerate
+    /**
+     * @param throttle How fast we are telling the robot to go.
+     * @param prevThrottle What the last throttle was.
+     * @param fullSpeedTime How long it should take to get to full speed *up for rephrasing
+     * @return Return how much to speed up.
+     */
     public static double accelLimit(double throttle, double prevThrottle, double fullSpeedTime) {
 
         double finalThrottle = throttle;
@@ -88,13 +119,20 @@ public final class utilities {
         return finalThrottle;
     }
 
-    //finds the difference between 2 angles
+    /**
+     * @param startingAngle What angle the robot is at.
+     * @param desiredAngle What angle you want the robot to be at.
+     * @return Returns the difference between the two angels.
+     */
     public static double angleDifference(double startingAngle, double desiredAngle) {
         double difference = desiredAngle - startingAngle;
         return angleConverter(difference);
     }
 
-    //converts angles to between -180 and 180
+    /**
+     * @param ang Angle being converted.
+     * @return Angle but from -180 to 180 instead of 0 to 360.
+     */
     private static double angleConverter(double ang) {
 
         ang = ang % 360.0;
@@ -107,7 +145,12 @@ public final class utilities {
         return (ang);
     }
 
-    //adjusts for wheels getting stuck
+    /**
+     * @param velocity    The current velocity of the motor system in feet per second
+     * @param stiction    The static friction of the motor system in throttle
+     * @param maxVelocity The max velocity of the motor system in feet per second
+     * @return throttle A throttle within the focused range of values which will actuate
+     */
     public static double adjustForStiction(double velocity, double stiction, double maxVelocity) {
 
         if (velocity > -0.001 && velocity < 0.001) {
