008633ff5a3aa28c84f6a0d6a57d631e04fc08ca
Jacob M <43586627+Jacobjme@users.noreply.github.com>
2019-01-19 10:05:32 -0600

    Intake rename
    I renamed the intakes and got rid of velcro 2

--- src/main/java/org/minutebots/robot/RobotMap.java
+++ src/main/java/org/minutebots/robot/RobotMap.java
@@ -2,10 +2,8 @@ package org.minutebots.robot;
 
 public class RobotMap {
     //Pneumatic control slots
-    public final static int INTAKE_1_1 = 1;
-    public final static int INTAKE_1_2 = 2;
-    public final static int INTAKE_2_1 = 3;
-    public final static int INTAKE_2_2 = 4;
+    public final static int INTAKE_1 = 1;
+    public final static int INTAKE_2 = 2;
     public final static int Cone_1 = 5;
     public final static int Cone_2 = 6;
 
