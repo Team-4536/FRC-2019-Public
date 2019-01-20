7b32999beab07b4bfce9e39c7855676f7daa71ea
Kevin Tran <26168327+Kinzerfest@users.noreply.github.com>
2019-01-20 11:36:51 -0600

    Moved robotmap

--- src/main/java/org/minutebots/robot/subsystems/Superstructure.java
+++ src/main/java/org/minutebots/robot/subsystems/Superstructure.java
@@ -1,59 +1,77 @@
 package org.minutebots.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Spark;
 import org.minutebots.lib.VirtualMotor;
 import org.minutebots.lib.VirtualSolenoid;
-import org.minutebots.robot.OI;
 
 public class Superstructure {
+    //Talons
+    private final int LEFT_FRONT_MOTOR = 2,
+            LEFT_BACK_MOTOR = 1,
+            RIGHT_FRONT_MOTOR = 0,
+            RIGHT_BACK_MOTOR = 3;
 
-    private static Superstructure instance = new Superstructure(RobotType.FRACTURE);
+    //Pneumatic Ports
+    private final static int INTAKE_1 = 1,
+            INTAKE_2 = 2,
+            CONE_1 = 3,
+            CONE_2 = 4;
+
+
+    //Victor Ports
+    private final static int SIDE_WHEEL = 0,
+            WHEEL = 1;
+
+    /*---------------------------------------Programmer's territory starts here----------------------------------*/
 
     Intake intake;
     CargoOuttake cargoOutake;
     DepotYeeter depotYeeter;
     Drivetrain driveTrain;
 
-    private Superstructure(RobotType robot){
-        switch(robot){
+    private static Superstructure instance = new Superstructure(RobotType.YEETER);
+
+    private Superstructure(RobotType robot) {
+        switch (robot) {
             case FRACTURE:
                 driveTrain = new Drivetrain(
-                        new Spark(OI.LEFT_FRONT_MOTOR),
-                        new Spark(OI.RIGHT_FRONT_MOTOR),
-                        new Spark(OI.LEFT_BACK_MOTOR),
-                        new Spark(OI.RIGHT_BACK_MOTOR));
+                        new Spark(LEFT_FRONT_MOTOR),
+                        new Spark(RIGHT_FRONT_MOTOR),
+                        new Spark(LEFT_BACK_MOTOR),
+                        new Spark(RIGHT_BACK_MOTOR));
                 cargoOutake = new CargoOuttake(
-                        new VirtualMotor(OI.WHEEL));
+                        new VirtualMotor(WHEEL));
                 depotYeeter = new DepotYeeter(
-                        new VirtualMotor(OI.SIDE_WHEEL));
+                        new VirtualMotor(SIDE_WHEEL));
                 intake = new Intake(
-                        new VirtualSolenoid(OI.INTAKE_1, OI.INTAKE_2),
-                        new VirtualSolenoid(OI.CONE_1, OI.CONE_2));
+                        new VirtualSolenoid(INTAKE_1, INTAKE_2),
+                        new VirtualSolenoid(CONE_1, CONE_2));
             default:
                 driveTrain = new Drivetrain(
-                        new Spark(OI.LEFT_FRONT_MOTOR),
-                        new Spark(OI.RIGHT_FRONT_MOTOR),
-                        new Spark(OI.LEFT_BACK_MOTOR),
-                        new Spark(OI.RIGHT_BACK_MOTOR));
+                        new WPI_TalonSRX(LEFT_FRONT_MOTOR),
+                        new WPI_TalonSRX(RIGHT_FRONT_MOTOR),
+                        new WPI_TalonSRX(LEFT_BACK_MOTOR),
+                        new WPI_TalonSRX(RIGHT_BACK_MOTOR));
                 cargoOutake = new CargoOuttake(
-                        new WPI_VictorSPX(OI.WHEEL));
+                        new WPI_VictorSPX(WHEEL));
                 depotYeeter = new DepotYeeter(
-                        new WPI_VictorSPX(OI.SIDE_WHEEL));
+                        new WPI_VictorSPX(SIDE_WHEEL));
                 intake = new Intake(
-                        new DoubleSolenoid(OI.INTAKE_1, OI.INTAKE_2),
-                        new DoubleSolenoid(OI.CONE_1, OI.CONE_2));
+                        new DoubleSolenoid(INTAKE_1, INTAKE_2),
+                        new DoubleSolenoid(CONE_1, CONE_2));
         }
     }
 
-    private enum RobotType{
+    private enum RobotType {
         FRACTURE,
         SIDEWINDER,
         YEETER
     }
 
-    static Superstructure getInstance(){
+    static Superstructure getInstance() {
         return instance;
     }
 }
