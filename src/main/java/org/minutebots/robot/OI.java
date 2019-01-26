7033431e598bcbb057eadbc3b2cc86aa969bbea6
abdulhamid <aali054@stpaul.k12.mn.us>
2019-01-26 12:03:03 -0600

    Created auto release command.
    We made it that when the button to extend the pistons is let go, the pistons go back in.

--- src/main/java/org/minutebots/robot/OI.java
+++ src/main/java/org/minutebots/robot/OI.java
@@ -12,14 +12,17 @@ public class OI {
             TOGGLE_TURN = 1,
             EXTEND_INTAKE = 5,
             RETRACT_INTAKE = 3,
+            //RELEASE_HATCH = 4,
             ADJUST_ANGLE_LEFT = 7,
             ADJUST_ANGLE_RIGHT = 8;
+
   
     /*---------------------------------------Programmer's territory starts here----------------------------------*/
     public static final Joystick primaryStick = new Joystick(PRIMARY_STICK_PORT);
     public static final JoystickButton trigger = new JoystickButton(primaryStick, TOGGLE_TURN),
             extend = new JoystickButton(primaryStick, EXTEND_INTAKE),
             retract = new JoystickButton(primaryStick, RETRACT_INTAKE),
+            //releaseHatch = new JoystickButton(primaryStick, RELEASE_HATCH),
             angleAdjustLeft = new JoystickButton(primaryStick, ADJUST_ANGLE_LEFT),
             angleAdjustRight = new JoystickButton(primaryStick, ADJUST_ANGLE_RIGHT);
 
@@ -27,7 +30,9 @@ public class OI {
         angleAdjustLeft.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(-3.0)));
         angleAdjustRight.whileHeld(new InstantCommand(() -> Drivetrain.getInstance().adjustAngle(3.0)));
         extend.whenPressed(Intake.extend());
-        retract.whenPressed(Intake.retract());
+        //retract.whenPressed(Intake.retract());
+        extend.whenReleased(Intake.retract());
+        //releaseHatch.whenPressed(Intake.releaseHatch());
     }
 
 }
