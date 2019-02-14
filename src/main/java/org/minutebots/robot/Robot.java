7c18329dccdd509c10df0185bc490dc5fb4e8601
a2332 <45018361+a2332@users.noreply.github.com>
2019-02-14 15:13:32 -0600

    Made it so it only cancles commands if one is running

--- src/main/java/org/minutebots/robot/Robot.java
+++ src/main/java/org/minutebots/robot/Robot.java
@@ -1,10 +1,18 @@
 package org.minutebots.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import org.minutebots.robot.commands.StrafeToVisionTarget;
 import org.minutebots.robot.hardwareconfigurations.*;
 import org.minutebots.robot.subsystems.Drivetrain;
+import org.minutebots.robot.subsystems.HatchPiston;
 import org.minutebots.robot.utilities.VisionCommunication;
+
+import java.util.Map;
 
 public class Robot extends TimedRobot {
 
@@ -22,6 +30,28 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+
+    ShuffleboardLayout drivetrainInfo = Shuffleboard.getTab("Debugging")
+            .getLayout("Drivetrain", BuiltInLayouts.kList);
+    drivetrainInfo.add("PID Controller", Drivetrain.getInstance().getPIDController());
+
+    ShuffleboardLayout drivetrainCommands = Shuffleboard.getTab("Debugging")
+            .getLayout("Drivetrain Commands", BuiltInLayouts.kList).withProperties(Map.of("Label position", "HIDDEN"));
+    drivetrainCommands.add("Current Commands", Drivetrain.getInstance());
+    if(Drivetrain.getInstance().getCurrentCommand() != null)
+      drivetrainCommands.add("Remove Current Command", new InstantCommand(() -> {Drivetrain.getInstance().getCurrentCommand().cancel();}));
+    drivetrainCommands.add(new InstantCommand("Set Setpoint 0",() -> Drivetrain.getInstance().setSetpoint(0)));
+    drivetrainCommands.add(new InstantCommand("Set Setpoint 90",() -> Drivetrain.getInstance().setSetpoint(90)));
+    drivetrainCommands.add(new InstantCommand("Set Setpoint 180",() -> Drivetrain.getInstance().setSetpoint(180)));
+    drivetrainCommands.add(new InstantCommand("Set Setpoint 270",() -> Drivetrain.getInstance().setSetpoint(270)));
+    drivetrainCommands.add(new StrafeToVisionTarget());
+
+    ShuffleboardLayout intake = Shuffleboard.getTab("Debugging")
+            .getLayout("Intake", BuiltInLayouts.kList).withProperties(Map.of("Label position", "HIDDEN"));
+    intake.add(HatchPiston.extend());
+    intake.add(HatchPiston.retract());
+    intake.add(HatchPiston.eject());
+
   }
 
   @Override
---
+++ src/main/java/org/minutebots/robot/commands/AutoRocket.java
@@ -0,0 +1,21 @@
+package org.minutebots.robot.commands;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import org.minutebots.robot.subsystems.Drivetrain;
+
+public class AutoRocket {
+
+    public AutoRocket(){
+
+        /*
+
+        move diagonally with 115 x and 59 y
+        use vision to align with the hatch
+        place hatch
+        move diagonally with 66.69 y and
+
+         */
+
+    }
+
+}
